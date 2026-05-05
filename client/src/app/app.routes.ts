0c00eb24300b4f09447cd0416802635297d36d3f
JustVolodymyr <daresdarased@gmail.com>
2026-05-05 18:30:33 +0300

    feat: implement header layout and styles

--- client/src/app/app.routes.ts
+++ client/src/app/app.routes.ts
@@ -1,3 +1,8 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    loadComponent: () => import('./shared/components/header/header').then((m) => m.Header),
+  },
+];
