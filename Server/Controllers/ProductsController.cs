d1817003c087092203b0ecffe80ce5c31f0c9b6a
JustVolodymyr <daresdarased@gmail.com>
2026-05-08 09:24:25 +0300

    feat: create Product entity, DbContext and ProductsController GET endpoints

---
+++ Server/Controllers/ProductsController.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Server.Data;
+using Server.Entities;
+
+namespace Server.Controllers
+{
+	[ApiController]
+	[Route("api/[controller]")]
+	public class ProductsController : ControllerBase
+	{
+		private readonly DataContext context;
+
+		public ProductsController(DataContext context)
+		{
+			this.context = context;
+		}
+
+		[HttpGet]
+		public async Task<ActionResult<IEnumerable<Product>>> GetProducts() {
+			var products = await context.Products.ToListAsync();
+			return Ok(products);
+		}
+
+		[HttpGet("{id}")]
+		public async Task<ActionResult<Product>> GetProduct(int id)
+		{
+			var product = await context.Products.FindAsync(id);
+			return Ok(product);
+		}
+	}
+}
---
+++ Server/Entities/Product.cs
@@ -0,0 +1,12 @@
+﻿namespace Server.Entities
+{
+	public class Product
+	{
+		public int Id { get; set; }
+		public string Name { get; set; } = null!;
+		public string? Description { get; set; }
+		public decimal BasePrice { get; set; }
+		public bool IsActive { get; set; } = true;
+		public DateTime CreatedAt { get; set; }
+	}
+}
