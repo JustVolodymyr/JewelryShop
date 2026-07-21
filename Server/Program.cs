03049af5550e6c627ad5b665467132eaffbca49c
JustVolodymyr <daresdarased@gmail.com>
2026-07-21 04:42:19 +0300

    chore: configure CORS, fetch-based HttpClient and SSR with system CA certs

--- Server/Program.cs
+++ Server/Program.cs
@@ -11,6 +11,7 @@ builder.Services.AddDbContext<DataContext>(options =>
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 //builder.Services.AddEndpointsApiExplorer();
 //builder.Services.AddSwaggerGen();
+builder.Services.AddCors();
 
 var app = builder.Build();
 
@@ -21,8 +22,9 @@ var app = builder.Build();
 //	app.UseSwaggerUI();
 //}
 
-//app.UseHttpsRedirection();
-
+app.UseHttpsRedirection();
+app.UseStaticFiles();
+app.UseCors(x => x.AllowAnyHeader().AllowAnyMethod().WithOrigins("http://localhost:4200","https://localhost:4200"));
 //app.UseAuthorization();
 
 app.MapControllers();
